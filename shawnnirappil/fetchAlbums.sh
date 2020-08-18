721ddf41ecb29cdbafed91a077bd6897a6c6b201
Shawn Nirappil <snirappil@appirio.com>
2020-08-18 16:07:43 -0500

    automate album fetching

--- shawnnirappil/fetchAlbums.sh
+++ shawnnirappil/fetchAlbums.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=./shawnnirappil/src/static/
+DIR=./src/static/
 FILE=albums.jpg
 cd $DIR
 curl -sSL -o ${FILE} 'https://tapmusic.net/collage.php?user=beat80&type=1month&size=3x3&caption=true'
